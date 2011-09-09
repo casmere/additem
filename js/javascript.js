6522e6b1dd13043e1c9d9ada19d66f07c3258404
Scott C. Shamka <casmere@me.com>
2011-09-08 20:37:43 -0400

    a

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,11 @@
 // Term 1108
 // Scott C. Shamka
 
+var clearLink = document.getElementById('clear');
+var anchorTags = clearLink.getElementsByTagName('a');
+
 function getItems(){
-	if(localStorage.getItem('name')){
+	if(localStorage.getItem('appName')){
 		var name = localStorage.getItem('appName');
 		var category = localStorage.getItem('appCategory');
 		var quantity = localStorage.getItem('appQuantity');
@@ -22,18 +25,31 @@ function getItems(){
 		
 		];
 		
-		alert("Something", data);
+		alert(data);
+		
+		document.getElementById('container').style.display = "none";
+		var clearLink = document.getElementById('clear');
+		clearLink.style.display = "block";
+		
+	} else {
 	
+		var name = 'enter your name';
+		var date = 'date';
+		var notes = 'enter any extra notes here!';
+		
+		document.getElementById('appName');
+		document.getElementById('appDate');
+		document.getElementById('appNotes');
 	}
 }
 
 function storeItems(id){
-	var name = document.getElementsByName('name').value;
-	var category = document.getElementsByName('category').value;
-	var quantity= document.getElementsByName('quantity').value;
-	var priority = document.getElementsByName('priority').value;
-	var date = document.getElementsByName('date').value;
-	var notes = document.getElementsByName('notes').value;
+	var name = document.getElementById('name').value;
+	var category = document.getElementById('category').value;
+	var quantity= document.getElementById('quantity').value;
+	var priority = document.getElementById('priority').value;
+	var date = document.getElementById('date').value;
+	var notes = document.getElementById('notes').value;
 	
 	localStorage.setItem('appName', name);
 	localStorage.setItem('appCategory', category);
@@ -47,3 +63,20 @@ function clearLocal(){
 	localStorage.clear();
 	return false;
 }
+
+function addOption(selectbox,text,value )
+{
+	var optn = document.createElement("OPTION");
+	optn.text = text;
+	optn.value = value;
+	selectbox.options.add(optn);
+}
+
+function addOption_list(selectbox){
+var forList = new Array("Home","School","Work","Other");
+for (var i=0; i < forList.length;++i){
+
+addOption(document.addItem.category_list, forList[i], forList[i]);
+}
+}
+		
