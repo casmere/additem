5f270dbb24eee73af03de9b05d67893abf54dd36
Scott C. Shamka <casmere@me.com>
2011-09-23 20:02:55 -0400

    lots of stuff

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Week 2 - VFW
+// Week 4 - VFW
 // Term 1108
 // Scott C. Shamka
 
@@ -21,58 +21,134 @@ function getLocalStorage() {
 }
 
 function getItems(){
-	if(db.getItem('appEvent')){
-		var event 		= db.getItem('appEvent');
-		var category 	= db.getItem('appCategory');
-		var date 		= db.getItem('appDate');
-		var notes 		= db.getItem('appNotes');
+	var getListdiv = document.getElementById("dataList");
+
+	for(var i=0, len = db.length; i < len; i++){
 		
-		var data = [
+		var key = db.key(i);
+		var value = db.getItem(key);
+		value = value.split(',');
+		
+		var category = value[0];
+		var event = value[1];
+		var date = value[2];
+		var notes = value[3];
+				
+		var newDiv = document.createElement("div");
+		
+			for(var ii=0, allLength = value.length; ii < allLength; ii++){
+				var newPara = document.createElement("p");
+				var itemTxt = document.createTextNode(value[ii]);
+				newPara.appendChild(itemTxt);
+				newDiv.appendChild(newPara);
+				getListdiv.appendChild(newDiv);
+			}
+				
+			var catImage = "other.png";
 			
-			category,		
-			event,
-			date,
-			notes
-		
-		];
-
-		showData(data);
-	}
-}
-
-function showData(data){
-		
-		document.getElementById('form1').style.display = "none";
+			if(category == "Home"){ catImage = "home.png"; }
+			if(category == "School"){ catImage = "school.jpg"; }
+			if(category == "Work"){ catImage = "work.jpg"; }
+			if(category == "Other"){ catImage = "other.png"; }
 	
-		var showDb = document.getElementById('showData'); 
-			showDb.style.display = "block";
-			
-		var dataList = document.getElementById('dataList'); 	// Accessing the unordered list Node
 		
-			 						
-		// Loop through the data array and list each piece of the array into an array element.
-		for(var i = 0;i < data.length;i++){
-		
-			var newListElement = document.createElement('li'); 	// Creating a "floating" list element
-				dataList.appendChild(newListElement); 			// Attaching the list element to the UL node
-			
-			var listData = document.createTextNode(data[i]);	// Creates a text node for each piece of the array
-				newListElement.appendChild(listData);			// Attaches the text node to the li element
-			
-		};
-}
+		//add image
+		var newP = document.createElement("p");
+		var newImg = document.createElement("IMG");
+		newImg.setAttribute("src", "images/" + catImage);
+		newP.appendChild(newImg);
 
+		//delete single item link
+		var deleteLink = document.createElement("a");
+		var setHref = deleteLink.setAttribute("href", "#");
+		var setOnclick = deleteLink.setAttribute("onclick", "deleteItem(" + key + ");");
+		var deleteText = document.createTextNode("delete item");
+		deleteLink.appendChild(deleteText);
+		newP.appendChild(deleteLink);
+
+		//edit single item link
+		var editLink = document.createElement("a");
+		var setHref = editLink.setAttribute("href", "#");
+		var setOnclick = editLink.setAttribute("onclick", "editItem(" + key + ");");
+		var editText = document.createTextNode("edit item");
+		editLink.appendChild(editText);
+		newP.appendChild(editLink);
+		getListdiv.appendChild(newP);
+		}
+	}
+	
 function saveData(id){	
 	var event 		= document.getElementById('event').value;
 	var category 	= document.getElementById('category').value;
 	var date 		= document.getElementById('date').value;
 	var notes	 	= document.getElementById('notes').value;
 	
-	db.setItem('appEvent', event);
-	db.setItem('appCategory', category);
-	db.setItem('appDate', date);
-	db.setItem('appNotes', notes);
+	var d = new Date();
+    var key= (d.getTime());
+    
+   	
+	var allItems = [
+			category,
+			event,
+			date,
+			notes
+		];
+		
+	db.setItem(key, allItems.join(';'));
 	
+	}
+
+function editItem(id){
+	var value = db.getItem(id);
+	var itemId = id;
+	
+	value = value.split(';');
+	var category = value[0];
+	var event = value[1];
+	var date = value[2];
+	var notes = value[3];
+	
+	//populates form fields with current localStorage values
+	document.getElementById('category').value = category;
+	document.getElementById('event').value = event;
+	document.getElementById('date').value = date;
+	document.getElementById('notes').value = notes;
+	
+	// reveal the editItem button
+	var editItem = document.getElementById('editItem');
+	editItem.style.display = "block";
+	var submit = document.getElementById('submit');
+	submit.style.display = "none";
+	
+	// capture editItem button's click
+	document.getElementById('editItem').onClick = function () {
+		var category = document.getElementById('category').value;
+		var event = document.getElementById('event').value;
+		var date = document.getElementById('date').value;
+		var notes = document.getElementById('notes').value;
+		
+		var allItems = [
+			category,
+			event,
+			date,
+			notes
+		];
+		if (event !== ""){
+			db.setItem(itemId, allItems.join(';'));
+		}else{
+			alert("Atleast an Event Title is required.");
+		}
+	};
+}
+
+function deleteItem(id){
+	var ask = confirm("Are you sure?");
+		if(ask){
+			db.removeItem(id);
+			window.location.reload();
+		}else{
+			alert("Item not removed.");
+		}
 }
 
 function clearLocal(){
@@ -88,6 +164,7 @@ function addOption(selectbox,text,value)
 		selectbox.options.add(optn);
 }
 
+// Category list
 function addOption_list(selectbox){
 	var forList = new Array("Home","School","Work","Other");
 		for (var i=0; i < forList.length;++i){
@@ -111,7 +188,7 @@ function validateForm(){
 	
 		saveData();
 		alert("Form Submitted!");
-		getItems();
+
 		
 
 }
